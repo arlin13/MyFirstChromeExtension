951dec7fb0847b3ee47661e167bfb6c2809c49db
Arlin Grijalba <arlin.grijalba@greatcall.com>
2017-09-07 10:09:00 -0700

    Added content to popup html and new javascript file just to show the date and time

---
+++ index.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//
+//   var date = new Date();
+//   $('#date').text = date;
+//
+// });
+
+var date = new Date();
+var time = date.getTime();
+var timeWorking = new Date();
+
+document.getElementById('date').innerHTML = date;
